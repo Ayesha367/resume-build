32d41ef09c359854fa6ad64be168241b8a00e73c
Ayesha Farooq <160249473+Ayesha367@users.noreply.github.com>
2025-02-02 18:05:44 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,8 @@
+const nameField = document.getElementById('name') as HTMLInputElement;
+const roleField = document.getElementById('role') as HTMLInputElement;
+const profileField = document.getElementById('profile') as HTMLTextAreaElement;
+
+document.getElementById('form')?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Dynamic Updates (Optional)
+});
